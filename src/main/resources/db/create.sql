33bda637b6b6190127959cde8031939fda6ef468
MarkMwago2 <markmwago@gmail.com>
2019-07-09 09:57:07 +0300

    create the DAO for the classes

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS departments (
 CREATE TABLE IF NOT EXISTS news (
  id int PRIMARY KEY auto_increment,
  name VARCHAR
+ departmentId INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS users (
