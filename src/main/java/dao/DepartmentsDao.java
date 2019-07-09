3d9723a3c2dba8a0adfbcf22d1af0f2e1246f103
MarkMwago2 <markmwago@gmail.com>
2019-07-09 11:14:02 +0300

    create the Dao interface for the main classes

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,4 +1,26 @@
 package dao;
 
-public class DepartmentsDao {
+
+
+
+import models.Departments;
+
+import java.util.List;
+
+public interface DepartmentsDao {
+    //create
+    void add (Departments department);
+    // void addDepartmentToNews(Department department, News news)
+
+    //read
+    List<Departments> getAll();
+    Departments findById(int id);
+    // List<News> getAllNewsForADepartment(int departmentId);
+
+    //update
+    void update(int id, String name, String description, int numberOfEmployees);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,24 @@
 package dao;
 
-public class NewsDao {
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+    //create
+    void add(News news);
+    //void addNewsToDepartment(News news, Department department);
+
+    //read
+    List<News> getAll();
+    // List<Department> getAllDepartmentsForANews(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
+
+
